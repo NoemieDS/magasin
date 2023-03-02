6493cae96c49ba00694940b3c83991c7cffe15c3
Noémie dS <112022872+NoemieDS@users.noreply.github.com>
2023-03-01 19:24:38 -0500

    Retrait du compteur, édition des commentaires

--- src/Appli.jsx
+++ src/Appli.jsx
@@ -8,7 +8,7 @@ function Appli() {
   
   /****************************************************************************** exemples JS */
   /* sans react serait ..... id produit, prix, quantité mis par l'utilisateur
-dans un objet JS / tableau associatif, pas un array avec position []
+  dans un objet JS / tableau associatif, pas un array avec position []
   panier = {
     prd0001: {prix: 15, qte: 12},
     prd0002: {prix: 15, qte: 02},
@@ -21,36 +21,28 @@ dans un objet JS / tableau associatif, pas un array avec position []
   //On utilise la fonction HOOK useState({}) pour surveiller l'état du panier
   //useState({}) Retourne un tableau avec 2 valeurs, un objet et une fonction
 
-
   //on part un objet vide pour y mettre les articles choisis par l'utilisateur
   /*
   let panier= etatPanier[0]; // c'est le {} dans le useState({})
   let setPanier = etatPanier[1]; //position 1 est une fonction qui surveille l'état
 */
 
-
-
-  /* peut être écrit en une ligne 
-    on utilise const car on ne modifie pas la variable d'état (aurait pu être let 
-    mais c pas la manière de faire)*/
-  //je veux maintenir le panier dans un objet JS surveillé par React
-
+/* On utilise const car on ne modifie pas la variable d'état (aurait pu être let 
+    mais c pas la manière de faire react)*/
+//je veux maintenir le panier dans un objet JS surveillé par React
     const [panier, setPanier] = useState({});
-
-    /* compteur de clic */
-    const [compteur, setCompteur] = useState(0);
-
- /******************************************************************************* localStorage */   
+    
+    /******************************************************************************* localStorage */   
     //Fonction de rappel
     //Action de sauvegarder dans le panier
     //useEffect prend 2 param :un call back et un tableau de dépendance
-/******************************************************************************** useEffect */   
+    /******************************************************************************** useEffect */   
     useEffect(
       () => localStorage.setItem('panier-ls', JSON.stringify(panier))
       , [panier] //le tableau des dépendances / mettre nom de la variable d'état de useState
-     ); //sans le tableau des dépendances, le useEffect du compteur de clic va augmenter !
+      ); //sans le tableau des dépendances, le useEffect du compteur de clic va augmenter !
 
-/***************** Local Storage démo au long **** */
+/***************** Local Storage démo au long sans useEffect() **** */
   //Récupérer le panier du local storage
 /*   const [panier, setPanier] = useState({
     function() {
@@ -63,7 +55,7 @@ dans un objet JS / tableau associatif, pas un array avec position []
     },
   });
  */
-/*   //EN plus simple
+/*   //Ecriture plus simple
   const [panier, setPanier] = useState(function() {
           return JSON.parse(localStorage.getItem('panier-ls')) || {};
 });
@@ -83,7 +75,6 @@ dans un objet JS / tableau associatif, pas un array avec position []
     <div className="Appli">
       {/* <Entete panier={panier} ali={'baba'} chose='been'/> */}
       <Entete panier={panier} />
-      <button onClick={() => setCompteur(compteur + 1)}>Compteur de clic ({compteur})</button>
       <ListeProduits panier={panier} setPanier={setPanier} />
       <Pied2page />
     </div>
